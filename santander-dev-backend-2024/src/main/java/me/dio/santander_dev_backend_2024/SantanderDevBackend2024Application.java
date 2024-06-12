628a94fc8520b129bf271fd9a38ad2c99d6714a5
GasanSP <genivaldo.alcan@gmail.com>
2024-06-12 20:11:24 -0300

    Initial commit

---
+++ santander-dev-backend-2024/src/main/java/me/dio/santander_dev_backend_2024/SantanderDevBackend2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_backend_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevBackend2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevBackend2024Application.class, args);
+	}
+
+}
---
+++ santander-dev-backend-2024/src/test/java/me/dio/santander_dev_backend_2024/SantanderDevBackend2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_backend_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevBackend2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
