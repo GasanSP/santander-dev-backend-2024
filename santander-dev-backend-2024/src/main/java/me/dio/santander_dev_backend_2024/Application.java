a9cabbf99a04a4290ea473710d62ace461a4b281
GasanSP <genivaldo.alcan@gmail.com>
2024-06-13 21:22:09 -0300

    Implementação da camada de dominio.

--- santander-dev-backend-2024/src/main/java/me/dio/santander_dev_backend_2024/Application.java
+++ santander-dev-backend-2024/src/main/java/me/dio/santander_dev_backend_2024/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevBackend2024Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevBackend2024Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
