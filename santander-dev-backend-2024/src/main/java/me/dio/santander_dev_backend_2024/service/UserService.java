aeef1d820938b4752116b8c4c3b79f26cfc4540e
GasanSP <genivaldo.alcan@gmail.com>
2024-06-18 16:09:09 -0300

    Primeira tentativa de publicação no Railway

---
+++ santander-dev-backend-2024/src/main/java/me/dio/santander_dev_backend_2024/service/UserService.java
@@ -0,0 +1,9 @@
+package me.dio.santander_dev_backend_2024.service;
+
+import me.dio.santander_dev_backend_2024.model.User;
+
+public interface UserService {
+    User findById(Long id);
+
+    User create(User userToCreate);
+}
